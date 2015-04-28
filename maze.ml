14bc47c0d527224f775291129a9b099633c6771c
Lucie-Dispot <lucie.dispot@gmail.com>
2015-04-28 19:13:45 +0200

    open_door

--- maze.ml
+++ maze.ml
@@ -1,9 +1,26 @@
-(* Puts a different colour on each tile of the maze *)
+open Tile
 
-(*let rec initialize_maze maze i j w h =
-  if i = w - 1 then initialize_maze ((false, false, false, false, i + j * w)::maze) 0 (j + 1) w h
-	else if (i = w - 1) && (j = h - 1) then maze
-	else initialize_maze ((false, false, false, false, i + j * w)::maze) (i + 1) j w h *)
+let open_door maze i j w h = function
+	| North -> if j > -1 then
+		maze.(i + j * w) <- (true, get_s maze.(i + j * w),
+		get_e maze.(i + j * w), get_w maze.(i + j * w), get_id maze.(i + j * w))
+	| South -> if j < h then
+		maze.(i + j * w) <- (get_n maze.(i + j * w), true,
+		get_e maze.(i + j * w), get_w maze.(i + j * w), get_id maze.(i + j * w))
+	| East	-> if i < w then
+		maze.(i + j * w) <- (get_n maze.(i + j * w), get_s maze.(i + j * w),
+		true, get_w maze.(i + j * w), get_id maze.(i + j * w))
+	| West	-> if i > -1 then
+		maze.(i + j * w) <- (get_n maze.(i + j * w), get_s maze.(i + j * w),
+		get_e maze.(i + j * w), true, get_id maze.(i + j * w))
+
+let open_door_neighbour maze i j w h = function
+	| North ->	open_door (maze) (i) (j + 1) (w) (h) (South)
+	| South ->	open_door (maze) (i) (j - 1) (w) (h) (North)
+	| East	->	open_door (maze) (i + 1) (j) (w) (h) (West)
+	| West	->	open_door (maze) (i - 1) (j) (w) (h) (East)
+
+(* Puts a different colour on each tile of the maze *)
 
 let initialize_maze maze w h =
 	for i = 0 to w * h - 1 do
@@ -12,6 +29,4 @@ let initialize_maze maze w h =
 
 let create_maze w h =
 	let maze = Array.make (w * h) (false, false, false, false, 0) in
-	let test = initialize_maze maze w h in
-	if maze.(2) = (false, false, false, false, 2) then Printf.printf "success\n"
-	else Printf.printf "fail\n"
+	initialize_maze maze w h
--- tile.ml
+++ tile.ml
@@ -1,1 +1,9 @@
 type tile = (bool * bool * bool * bool * int)
+
+type dir = North | South | East | West
+
+let get_n (a, _, _, _, _) = a
+let get_s (_, a, _, _, _) = a
+let get_e (_, _, a, _, _) = a
+let get_w (_, _, _, a, _) = a
+let get_id (_, _, _, _, a) = a
