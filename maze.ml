7795f459d704e36f365bc0965d14b06aa7496e00
Lucie Dispot <dispot_a@epitech.eu>
2015-04-29 18:23:59 +0200

    Maze generation working

--- maze.ml
+++ maze.ml
@@ -1,72 +1,78 @@
 open Tile
 
-let open_door maze i j w h = function
+let open_door maze i j w h id = function
   | North -> if j > -1 then
-      maze.(i + j * w).id <- w * h;
+      maze.(i + j * w).id <- id;
       maze.(i + j * w).n <- true
   | South -> if j < h then
-      maze.(i + j * w).id <- w * h;
+      maze.(i + j * w).id <- id;
       maze.(i + j * w).s <- true
   | East	-> if i < w then
-      maze.(i + j * w).id <- w * h;
+      maze.(i + j * w).id <- id;
       maze.(i + j * w).e <- true
   | West	-> if i > -1 then
-      maze.(i + j * w).id <- w * h;
+      maze.(i + j * w).id <- id;
       maze.(i + j * w).w <- true
 
-let open_door_neighbour maze i j w h = function
-  | North ->	open_door (maze) (i) (j + 1) (w) (h) (South)
-  | South ->	open_door (maze) (i) (j - 1) (w) (h) (North)
-  | East	->	open_door (maze) (i + 1) (j) (w) (h) (West)
-  | West	->	open_door (maze) (i - 1) (j) (w) (h) (East)
+let open_door_neighbour maze i j w h id = function
+  | North ->	open_door (maze) (i) (j - 1) (w) (h) (id) (South)
+  | South ->	open_door (maze) (i) (j + 1) (w) (h) (id) (North)
+  | East	->	open_door (maze) (i + 1) (j) (w) (h) (id) (West)
+  | West	->	open_door (maze) (i - 1) (j) (w) (h) (id) (East)
 
-(* Check if door can be open (not already open & within the maze's bounds) *)
+(* Check if door can be open (within the maze's bounds) *)
 
 let open_random_door maze w h =
 	let i = Random.int w
 	and j = Random.int h
 	and dir = Random.int 4 in
+  if maze.(i + j * w).id = h * w then 0
+  else
   match dir with
     | 0 -> if j = 0 then 0
-      else if maze.(i + j * w).n = true then 0
+      else if maze.(i + j * w).id = maze.(i + j * w - w).id then 0
       else
       begin
-        open_door maze i j w h North;
-        open_door_neighbour maze i j w h North;
+        open_door maze i j w h maze.(i + j * w).id North;
+        open_door_neighbour maze i j w h maze.(i + j * w).id North;
         1;
       end
     | 1 -> if j = h - 1 then 0
-      else if maze.(i + j * w).s = true then 0
+      else if maze.(i + j * w).id = maze.(i + j * w + w).id then 0
       else
       begin
-        open_door maze i j w h South;
-        open_door_neighbour maze i j w h South;
+        open_door maze i j w h maze.(i + j * w).id South;
+        open_door_neighbour maze i j w h maze.(i + j * w).id South;
         1;
       end
     | 2 -> if i = w - 1 then 0
-      else if maze.(i + j * w).e = true then 0
+      else if maze.(i + j * w).id = maze.(i + 1 + j * w).id then 0
       else
       begin
-        open_door maze i j w h East;
-        open_door_neighbour maze i j w h East;
+        open_door maze i j w h maze.(i + j * w).id East;
+        open_door_neighbour maze i j w h maze.(i + j * w).id East;
         1;
       end
     | 3 -> if i = 0 then 0
-      else if maze.(i + j * w).w = true then 0
+      else if maze.(i + j * w).id = maze.(i - 1 + j * w).id then 0
       else
       begin
-        open_door maze i j w h West;
-        open_door_neighbour maze i j w h West;
+        open_door maze i j w h maze.(i + j * w).id West;
+        open_door_neighbour maze i j w h maze.(i + j * w).id West;
         1;
       end
+    | _ -> 0
+
+(* Converted represents the number of tiles converted *)
 
 let generate_maze maze w h =
-  let converted = 0 in
-    while converted < w * h do
-      converted = converted + (open_random_door maze w h)
+  let converted = ref 0
+  and max = ref ((w * h) - 1) in
+    while converted < max do
+      if open_random_door maze w h = 1 then converted := !converted + 1;
     done
 
-  (* Puts a different colour on each tile of the maze *)
+(* Puts a different colour on each tile of the maze *)
 
 let initialize_maze maze w h =
   for i = 0 to w * h - 1 do
--- step1.ml
+++ step1.ml
@@ -4,9 +4,10 @@ let _ =
 	try
 		let width = int_of_string Sys.argv.(1)
 		and height = int_of_string Sys.argv.(2)
-	in if width > 0 && height > 0 && width < 1000 && height < 1000
+	in if width > 1 && height > 1 && width < 1000 && height < 1000
 	  then let maze = Maze.create_maze width height in Printf.printf "success\n"
-	else raise (Invalid_argument "width and height must be positive and less than 1000\n")
+	else raise (Invalid_argument "wrong input")
 	with
 		| Failure ("int_of_string") -> Printf.eprintf "%s\n" usage_msg
 		| Invalid_argument ("index out of bounds") -> Printf.eprintf "%s\n" usage_msg
+		| Invalid_argument ("wrong input") -> Printf.eprintf "width and height must be positive and less than 1000\n"
