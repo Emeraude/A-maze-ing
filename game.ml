228d5319b5a69b28b463d856c2e0ea6b6db1ac6b
Thibaut Broggi <broggi_t@epitech.eu>
2015-05-02 23:08:08 +0200

    nazis are all created at position 0;0

--- game.ml
+++ game.ml
@@ -1,11 +1,20 @@
+open Maze
+
 type game = {nazis: int * int list;
 	     juif: int * int;
 	     camp: int * int;
 	     pieces: int * int list;
 	     teleporters: int * int list}
 
-(* let create_nazis maze = *)
-(*   |  *)
+let int_sqrt n =
+  int_of_float (sqrt (float_of_int n))
+
+let create_nazis maze =
+  let rec _create_nazis list n = match n with
+    | 0 -> []
+    | _ -> _create_nazis ((0, 0)::list) (n - 1)
+  in _create_nazis [] ((int_sqrt (maze.height * maze.width)) / 10 + 1)
+
 
 (* let init_game maze = *)
   
