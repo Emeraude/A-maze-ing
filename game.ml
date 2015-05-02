84a9d3c71334be5e38310c8426a7716b83570632
Thibaut Broggi <broggi_t@epitech.eu>
2015-05-02 22:48:52 +0200

    adding step5's rule in makefile and game type

---
+++ game.ml
@@ -0,0 +1,11 @@
+type game = {nazis: int * int list;
+	     juif: int * int;
+	     camp: int * int;
+	     pieces: int * int list;
+	     teleporters: int * int list}
+
+(* let create_nazis maze = *)
+(*   |  *)
+
+(* let init_game maze = *)
+  
