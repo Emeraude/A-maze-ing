7c4b261a903089d08078ad11afa63e28f2fbb3f1
Thibaut Broggi <broggi_t@epitech.eu>
2015-05-02 03:28:54 +0200

    adding openDoor and closeDoor functions in Door module

--- door.ml
+++ door.ml
@@ -3,3 +3,11 @@ type door = Opened | Closed
 let isOpened = function
   | Opened -> true
   | Closed -> false
+
+let openDoor = function
+  | Opened -> raise (Invalid_argument "Door already opened")
+  | Closed -> Opened
+
+let closeDoor = function
+  | Opened -> Closed
+  | Closed -> raise (Invalid_argument "Door already closed")
