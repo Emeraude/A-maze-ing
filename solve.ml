50ba2865e15d613ff9f155cf30bce812f9249307
Lucie Dispot <dispot_a@epitech.eu>
2015-05-03 20:15:43 +0200

    solve now returns the path to take

--- solve.ml
+++ solve.ml
@@ -1,15 +1,15 @@
 open Tile
 open Maze
 
-exception Found
+exception Found of int list
 
 (* On met les id à -1 si on est sur le chemin vers l'arrivée *)
 
 let rec solve maze curr last finish =
-  if curr < 0 || curr >= maze.width * maze.height || last < 0 || last >= maze.width * maze.height then false
+  if curr < 0 || curr >= maze.width * maze.height || last < 0 || last >= maze.width * maze.height then raise Not_found
   else if curr = finish then begin
     maze.tiles.(curr).id <- -1;
-    true;
+    [];
   end
   else let i = curr mod maze.width
   and j = curr / maze.width in
@@ -18,11 +18,12 @@ let rec solve maze curr last finish =
       begin fun n d ->
         let next = (i + dirs.(n).x + (j + dirs.(n).y) * maze.width) in
         if   Door.is_opened d && next != last
-    	  && solve maze next curr finish
-    	then raise Found
+    	then try let path = solve maze next curr finish in
+    	raise (Found (next::path))
+	    with Not_found -> ()
       end
       (access maze i j).doors;
-    false
-  with Found ->
+    raise Not_found
+  with Found path ->
     maze.tiles.(curr).id <- -1;
-    true
+    path
