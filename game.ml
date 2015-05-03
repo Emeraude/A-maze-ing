3cb31aea241c9d3333fed54c6307f67707099f6d
Thibaut Broggi <broggi_t@epitech.eu>
2015-05-03 22:49:59 +0200

    adding score and very basic score
           fix exception on nazi = jew

--- game.ml
+++ game.ml
@@ -5,7 +5,8 @@ type game = {nazis: (int * int) list;
 	     jew: int * int;
 	     camp: int * int;
 	     pieces: (int * int) list;
-	     teleporters: (int * int) list}
+	     teleporters: (int * int) list;
+	     score: int}
 
 let images = [|
   Sdlloader.load_image "./images/camp.png";
@@ -41,19 +42,29 @@ let create_teleporters maze =
     | _ -> (random_pos maze)::(_create_teleporters (n - 1))
   in _create_teleporters ((int_sqrt (maze.height * maze.width)) / 5 + 2)
 
-let init_game maze =
+let init_game maze score =
   {nazis = create_nazis maze;
    jew = create_jew maze;
    camp = create_camp maze;
    pieces = [];
-   teleporters = create_teleporters maze}
+   teleporters = create_teleporters maze;
+   score = score}
+
+let increase_score game nb =
+  {nazis = game.nazis;
+   jew = game.jew;
+   camp = game.camp;
+   pieces = game.pieces;
+   teleporters = game.teleporters;
+   score = game.score + nb}
 
 let jew_move game pos =
   {nazis = game.nazis;
    jew = pos;
    camp = game.camp;
    pieces = game.pieces;
-   teleporters = game.teleporters}
+   teleporters = game.teleporters;
+   score = game.score}
 
 let move_right maze game = match game.jew with
   | (x, y) -> if Door.is_opened (Maze.access maze x y).doors.(2) then jew_move game (x + 1, y) else game
@@ -75,6 +86,15 @@ let check_tp game = match List.filter (fun a -> a = game.jew) game.teleporters w
   | [] -> game
   | _ -> take_teleporter game
 
+let check_coin game = match List.filter (fun a -> a = game.jew) game.pieces with
+  | [] -> game
+  | pos -> print_endline "Cling cling ! +10"; {nazis = game.nazis;
+						jew = game.jew;
+						camp = game.camp;
+						pieces = List.filter (fun a -> a != List.hd pos) game.pieces;
+						teleporters = game.teleporters;
+						score = game.score + 10}
+
 let jew_is_alive game = match List.filter (fun a -> a = game.jew) game.nazis with
   | [] -> true
   | _ -> false
@@ -86,10 +106,10 @@ let events maze game = function
     | Sdlevent.QUIT ->							exit 0
     | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_ESCAPE; _} ->	exit 0
     | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_q; _} ->		exit 0
-    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_RIGHT; _} ->		check_tp (move_right maze game)
-    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_UP; _} ->		check_tp (move_up maze game)
-    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_DOWN; _} ->		check_tp (move_down maze game)
-    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_LEFT; _} ->		check_tp (move_left maze game)
+    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_RIGHT; _} ->		check_coin (check_tp (move_right maze game))
+    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_UP; _} ->		check_coin (check_tp (move_up maze game))
+    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_DOWN; _} ->		check_coin (check_tp (move_down maze game))
+    | Sdlevent.KEYDOWN {Sdlevent.keysym=Sdlkey.KEY_LEFT; _} ->		check_coin (check_tp (move_left maze game))
     | _ ->								game
 
 let rec get_events maze game =
@@ -130,20 +150,30 @@ let two_to_one_pos maze = function
 let one_to_two_pos maze = function
   | x -> (x mod maze.width, x / maze.width)
 
-let move_nazis maze game t = match t mod 5 with
+let move_nazis maze game t = try match t mod 7 with
   | 0 -> {nazis = List.map (fun a -> one_to_two_pos maze (List.hd (Solve.solve maze (two_to_one_pos maze a) (two_to_one_pos maze a) (two_to_one_pos maze game.jew)))) game.nazis;
-       jew = game.jew;
-       camp = game.camp;
-       pieces = game.pieces;
-       teleporters = game.teleporters}
+	  jew = game.jew;
+	  camp = game.camp;
+	  pieces = game.pieces;
+	  teleporters = game.teleporters;
+	  score = game.score}
+  | _ -> game
+  with Failure ("hd") -> game
+
+let add_pieces maze game t = match t mod 16 with
+  | 0 -> {nazis = game.nazis;
+	  jew = game.jew;
+	  camp = game.camp;
+	  pieces = (random_pos maze)::game.pieces;
+	  teleporters = game.teleporters;
+	  score = game.score}
   | _ -> game
 
 let rec game_loop screen maze game t =
   begin
     let game = get_events maze game in
     Sdltimer.delay 50;
-    let game = move_nazis maze game t in
-    (* faire apparaitre les pieces *)
+    let game = add_pieces maze (move_nazis maze game t) t in
     Draw.draw_maze_tiles screen maze false;
     draw_sprites screen maze game;
     if Sdlmixer.playing_music ();
@@ -154,14 +184,17 @@ let rec game_loop screen maze game t =
     then
       game_loop screen maze game (t + 1)
     else
-      jew_is_alive game && end_is_reached game
+      match jew_is_alive game && end_is_reached game with
+	| true -> print_endline "Welcome at home ! +200"; true
+	| false -> false
   end
 
 let rec new_level screen width height = function
   | 0 -> ()
   | lvl -> begin
     let maze = Maze.create_maze width height 0 in
-    if game_loop screen maze (init_game maze) 0 then
+    let game = init_game maze (3 - lvl * 200) in
+    if game_loop screen maze game 0 then
       new_level screen width height (lvl - 1)
   end
 
